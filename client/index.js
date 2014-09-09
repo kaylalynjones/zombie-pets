b487cc7e906e4c891737fbf21d75e0f371a564f8
Kayla Jones <KaylaLyne424@gmail.com>
2014-09-09 14:59:23 -0500

    create a pet, make pet player1 or player 2

--- client/index.js
+++ client/index.js
@@ -3,7 +3,41 @@
 
   angular.module('zombie-pets', [])
   .controller('MainController', ['$scope', function($scope){
+
     $scope.title = 'Zombie Pets';
+    $scope.weapon = {};
+    $scope.weapons = [];
+    $scope.pet = {health:100};
+    $scope.pets = [];
+    //player1 player2
+    $scope.player1 = null;
+    $scope.player2 = null;
+
+    $scope.toggleWeapon = function(){
+      $scope.hideWeapon = !!!$scope.hideWeapon;
+    };
+
+    $scope.togglePet = function(){
+      $scope.hidePet = !!!$scope.hidePet;
+    };
+
+    $scope.addWeapon = function(){
+      $scope.weapons.push($scope.weapon);
+      $scope.weapon = {};
+      $('#name').focus();
+    };
+
+    $scope.addPet = function(){
+      var index = $scope.pet.weapon * 1;
+      $scope.pet.weapon = $scope.weapons[index];
+      $scope.pets.push($scope.pet);
+      $scope.pet = {health:100};
+    };
+
+    $scope.setPlayer = function(num){
+      $scope['player' + num] = this.p;
+    };
+
   }]);
 
 })();
