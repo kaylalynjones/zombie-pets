5ad5559cb907199aebe2bd993f27df4baff3ac48
Kayla Jones <KaylaLyne424@gmail.com>
2014-09-09 20:12:59 -0500

    fights, reduces health turns into zombie, health progress bar

--- client/index.js
+++ client/index.js
@@ -7,7 +7,10 @@
     $scope.title = 'Zombie Pets';
     $scope.weapon = {};
     $scope.weapons = [];
-    $scope.pet = {health:100};
+    $scope.pet = {
+      isZombie: false,
+      health:100
+    };
     $scope.pets = [];
     //player1 player2
     $scope.player1 = null;
@@ -31,13 +34,44 @@
       var index = $scope.pet.weapon * 1;
       $scope.pet.weapon = $scope.weapons[index];
       $scope.pets.push($scope.pet);
-      $scope.pet = {health:100};
+      $scope.pet = {
+        isZombie: false,
+        health:100
+      };
     };
 
     $scope.setPlayer = function(num){
       $scope['player' + num] = this.p;
     };
 
+    $scope.fight = function(){
+      var i = Math.floor(Math.random()*2 +1),
+          petX = $scope['player' + i],
+          petY = $scope['player' + (i === 1 ? 2 : 1)];
+      attack(petX, petY);
+      attack(petY, petX);
+    };
+
+    function attack(p1,p2){
+      var max, min;
+
+      if (p1.health < 0) {
+        max = 3;
+        min = 0;
+      }
+      else {
+        max = p1.weapon.damage;
+        min = 0;
+      }
+
+      var damage = Math.floor(Math.random() * (max - min + 1) + min);
+
+      p2.health -= damage;
+
+      if (p2.health < 0) {
+        p2.isZombie = true;
+      }
+    }
   }]);
 
 })();
